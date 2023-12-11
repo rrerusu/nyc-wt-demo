0e9a858b9540949fae7a58be184ac821ce765e6f
rrerusu <rrerusu@gmail.com>
2023-12-11 09:43:43 -0600

    Test Vue app in GitHub Pages

---
+++ NYC_WT_demo/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+async function sendText(content) {
+	try {
+		let request = await fetch("https://httpbin.org/post", {
+			method: 'POST',
+			content
+		});
+		document.getElementById('toxicityOutput').value = request.body;
+		// console.log(request);
+		return request || [];
+	} catch (error) {
+			console.error(error);
+	}
+}
+</script>
+
+<template>
+  <h3 id="demo">Test</h3>
+
+  <form>
+    <input id="inputText" name="inputText">
+    <button name="SubmitTextButton" type="button" onclick="sendText(document.getElementById('inputText').value)">Submit</button>
+    <p id="toxicityOutput"></p>
+  </form>
+</template>
